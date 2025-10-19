85743ed07b69b55e12666bda3fd26c3a62090649
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-10-19 09:40:44 +0200

    initial commit

---
+++ Formula/copilot-language-server.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class CopilotLanguageServer < Formula
+  desc "GitHub Copilot Language Server"
+  homepage "https://github.com/github/copilot-language-server"
+  url "https://registry.npmjs.org/@github/copilot-language-server/-/copilot-language-server-1.384.0.tgz"
+  sha256 "784d5b731969d2773a8af6c68d744579ba854c6288ff98b17dddafe2798cebad"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec), "@github/copilot-language-server"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/copilot-language-server", "--help"
+  end
+end
