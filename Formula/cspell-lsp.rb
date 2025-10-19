b251951a0afe59122594eb471580e801b1ac8584
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-10-19 17:30:03 +0200

    Support cspell-lsp

---
+++ Formula/cspell-lsp.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class CspellLsp < Formula
+  desc "A simple source code spell checker"
+  homepage "https://github.com/vlabo/cspell-lsp"
+  url "https://registry.npmjs.org/@vlabo/cspell-lsp/-/cspell-lsp-1.1.5.tgz"
+  sha256 "1338db0080360e2e2cd9af7d0444553f1929adddf31a554dd1ba78997d1588af"
+  license "GPL-3.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec), "@vlabo/cspell-lsp@1.1.5"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/cspell-lsp", "--help"
+  end
+end
--- README.txt
+++ README.txt
@@ -1,4 +1,5 @@
 brew tap samu/tools https://github.com/scristobal/homebrew-tools
 brew install samu/tools/copilot-language-server
 brew install samu/tools/typescript-language-server
+brew install samu/tools/cspell-lsp
 
